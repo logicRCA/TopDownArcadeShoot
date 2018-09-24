9330d83a95952ce0b6cfcbb3cea03a087f0125cf
Halim Lais <halimlais@hotmail.com>
2018-09-24 16:42:33 +0100

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(PlayerController))]
+public class Player : MonoBehaviour {
+
+    public float speed = 5f;
+    PlayerController controller;
+    void Start () {
+
+        controller = GetComponent<PlayerController>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        Vector3 moveVelocity = moveInput.normalized * speed;
+
+        controller.Move(moveVelocity);
+
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+    Vector3 mVelocity;
+    Rigidbody rigidBody;
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+
+    public void Move(Vector3 velocity)
+    {
+        mVelocity = velocity;
+    }
+    public void FixedUpdate()
+    {
+        rigidBody.MovePosition(rigidBody.position + mVelocity * Time.fixedDeltaTime);
+        
+    }
+}
